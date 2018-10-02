638191d7e105ab083c62e1d45c7f2f56ad6aa173
Hondy000 <kazu.1994.1014@gmail.com>
2018-10-03 01:08:15 +0900

    first commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+   
+    /// <brief> カメラのターゲット.</brief>
+    [SerializeField]
+    GameObject m_cameraTarget;
+
+    /// <brief> ターゲットの位置.</brief>
+    [SerializeField]
+    Camera m_camera;
+
+    /// <brief> ターゲットまでの距離.</brief>
+    [SerializeField]
+    float m_distance;
+
+   
+    [SerializeField]
+    Vector3 m_eulerRotation;  /// <brief> カメラ角度.</brief>
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // カメラ位置の更新
+        var _quaternion = Quaternion.Euler(m_eulerRotation);
+        var _rotateVector = _quaternion * new Vector3(0, 0, m_distance);
+        m_camera.transform.position = m_cameraTarget.transform.position + _rotateVector;
+
+        m_camera.transform.localRotation = _quaternion;
+
+    }
+}
---
+++ Assets/Scripts/VirtualInputManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualInputManager : MonoBehaviour {
+
+    class BaseInput
+    {
+        string m_name;
+
+        public string Name
+        {
+            get
+            {
+                return m_name;
+            }
+
+            set
+            {
+                m_name = value;
+            }
+        }
+    }
+
+    List<BaseInput> m_InputList;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_InputList.Clear();
+
+    }
+}
